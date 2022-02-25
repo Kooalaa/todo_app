c33bbdfe406074a9001bf90dd29ee6ce61bacace
skemoff <t9kesa00@students.oamk.fi>
2022-02-25 15:04:59 +0200

    Kelloapi

--- lib/apitest.dart
+++ lib/apitest.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future<Album> fetchAlbum() async{
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  final response = await http.get(Uri.parse(/*'https://jsonplaceholder.typicode.com/albums/1'*/'http://worldtimeapi.org/api/timezone/Europe/Helsinki'));
   if (response.statusCode == 200){
     return Album.fromJson(jsonDecode(response.body));
   } else {
@@ -12,19 +12,22 @@ Future<Album> fetchAlbum() async{
 }
 
 class Album {
-  final int userId;
-  final int id;
-  final String title;
+  //final int userId;
+  //final int id;
+  //final String title;
+  final String datetime;
 
   const Album({
-    required this.userId,
-    required this.id,
-    required this.title,
+    //required this.userId,
+    //required this.id,
+    //required this.title,
+    required this.datetime,
   });
 
   factory Album.fromJson(Map<String, dynamic> json){
-  return Album(userId: json['userId'],
+  return Album(/*userId: json['userId'],
               id: json['id'],
-              title: json['title'],);
+              title: json['title'],*/
+              datetime: json['datetime']);
     }
 }
--- lib/bottombar.dart
+++ lib/bottombar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class bottomBar extends StatelessWidget {
   const bottomBar({ Key? key }) : super(key: key);
 
@@ -9,6 +10,7 @@ class bottomBar extends StatelessWidget {
           shape: const CircularNotchedRectangle(),
           color: Color.fromARGB(255, 226, 0, 0),
           child: Container(height: 50.0)
+          
     );
   }
 }
